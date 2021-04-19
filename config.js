0e0a9f164545bd45d18acb29dfc147680f562d01
Byrne Hollander <byrnehollander@users.noreply.github.com>
2021-04-19 14:02:50 -0400

    Init

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://blog.byrnehollander.com',
+  pathPrefix: '/',
+  title: 'Byrne\'s Blog',
+  subtitle: 'Mostly programming thoughts, probably',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Byrne Hollander',
+    photo: '/photo.jpg',
+    bio: 'Programmer and product person living in NYC',
+    contacts: {
+      email: 'bhollander823@gmail.com',
+      github: 'byrnehollander',
+      linkedin: 'byrnehollander',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      rss: '',
+      vkontakte: '',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: ''
+    }
+  }
+};
